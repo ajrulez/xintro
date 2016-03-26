24de53375980d118b30ef0b274df9c11382580a0
Yonatan Kahana <yonatankahana.il@gmail.com>
2016-03-26 15:11:51 +0300

    trying travis 2.

--- example/src/main/java/com/github/yonatankahana/introexample/MainActivity.java
+++ example/src/main/java/com/github/yonatankahana/introexample/MainActivity.java
@@ -183,6 +183,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         } else if (id == R.id.nav_slideshow) {
 
+
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
